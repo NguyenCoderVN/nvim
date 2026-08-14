82ec8225c82df1ed490f6742165df1482f88595b
NguyenCoderVN <41292507+NguyenCoderVN@users.noreply.github.com>
2026-08-14 11:29:47 +0700

    Update parser-neorg.sh

--- install_sh/parser-neorg.sh
+++ install_sh/parser-neorg.sh
@@ -2,23 +2,20 @@
 
 set -e
 
-cd ~ || exit
-mkdir -p ~/.config/nvim/parser
-rm -rf /tmp/tree-sitter-norg /tmp/tree-sitter-norg-meta /tmp/tree-sitter-norg-table
+LAZY_DIR="$HOME/.local/share/nvim/lazy"
+PARSER_DIR="$HOME/.local/share/nvim/site/parser"
 
-git clone --recursive https://github.com/nvim-neorg/tree-sitter-norg /tmp/tree-sitter-norg
-cd /tmp/tree-sitter-norg || exit
+mkdir -p "$PARSER_DIR"
+
+# 1. Build norg.so từ mã nguồn Lazy
+cd "$LAZY_DIR/tree-sitter-norg" || exit
 gcc -fPIC -Os -I./src -c src/parser.c -o parser.o
 g++ -fPIC -Os -I./src -c src/scanner.cc -o scanner.o
-g++ -shared -fPIC parser.o scanner.o -o ~/.config/nvim/parser/norg.so
+g++ -shared -fPIC parser.o scanner.o -o "$PARSER_DIR/norg.so"
+rm -f parser.o scanner.o
 
-git clone https://github.com/nvim-neorg/tree-sitter-norg-meta /tmp/tree-sitter-norg-meta
-cd /tmp/tree-sitter-norg-meta || exit
-gcc -fPIC -Os -shared -I./src src/parser.c -o ~/.config/nvim/parser/norg_meta.so
-
-git clone https://github.com/nvim-neorg/tree-sitter-norg-table /tmp/tree-sitter-norg-table
-cd /tmp/tree-sitter-norg-table || exit
-gcc -fPIC -Os -shared -I./src src/parser.c -o ~/.config/nvim/parser/norg_table.so
+# 2. Build norg_meta.so từ mã nguồn Lazy
+cd "$LAZY_DIR/tree-sitter-norg-meta" || exit
+gcc -fPIC -Os -shared -I./src src/parser.c -o "$PARSER_DIR/norg_meta.so"
 
 cd ~ || exit
-rm -rf /tmp/tree-sitter-norg /tmp/tree-sitter-norg-meta /tmp/tree-sitter-norg-table
