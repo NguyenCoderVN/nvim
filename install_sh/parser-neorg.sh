1fe8e17ed454a007361dcdbd980a6f8f5b886d03
NguyenCoderVN <41292507+NguyenCoderVN@users.noreply.github.com>
2026-08-14 11:39:11 +0700

    Update parser-neorg.sh

--- install_sh/parser-neorg.sh
+++ install_sh/parser-neorg.sh
@@ -4,18 +4,22 @@ set -e
 
 LAZY_DIR="$HOME/.local/share/nvim/lazy"
 PARSER_DIR="$HOME/.local/share/nvim/site/parser"
+TS_QUERIES_DIR="$LAZY_DIR/nvim-treesitter/runtime/queries"
 
 mkdir -p "$PARSER_DIR"
 
-# 1. Build norg.so từ mã nguồn Lazy
+# 1. Build norg.so
 cd "$LAZY_DIR/tree-sitter-norg" || exit
 gcc -fPIC -Os -I./src -c src/parser.c -o parser.o
 g++ -fPIC -Os -I./src -c src/scanner.cc -o scanner.o
 g++ -shared -fPIC parser.o scanner.o -o "$PARSER_DIR/norg.so"
 rm -f parser.o scanner.o
 
-# 2. Build norg_meta.so từ mã nguồn Lazy
+# 2. Build norg_meta.so
 cd "$LAZY_DIR/tree-sitter-norg-meta" || exit
 gcc -fPIC -Os -shared -I./src src/parser.c -o "$PARSER_DIR/norg_meta.so"
+
+# 3. Copy norg queries
+cp -rf "$LAZY_DIR/neorg/queries/norg"* "$TS_QUERIES_DIR/"
 
 cd ~ || exit
